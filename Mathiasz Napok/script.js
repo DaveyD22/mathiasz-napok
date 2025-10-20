ac3f4aa6130fd49696799c94079f7a74e47792dc
KomariMate <matekomari27@gmail.com>
2025-10-20 19:35:16 +0200

    képszalag (2. verzió)

--- Mathiasz Napok/script.js
+++ Mathiasz Napok/script.js
@@ -12,12 +12,20 @@ function initializeSlider(){
     }
 }
 function showSlide(index){
+    if(index >= slides.length){
+        slideIndex = 0;
+    }
+    else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
     slides.forEach(slide => {
         slide.classList.remove("displaySlide")
     })
+    slides[slideIndex].classList.add("displaySlide")
 }
 function prevSlide(){
-
+    slideIndex--;
+    showSlide(slideIndex);
 }
 function nextSlide(){
     slideIndex++;
